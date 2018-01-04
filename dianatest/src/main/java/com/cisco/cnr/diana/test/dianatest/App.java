fb0335c27122f052c4db12364b9111a8d3446dd1
danielraja <danny2mar91@gmail.com>
2018-01-04 13:44:42 +0530

    developer 3

--- dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
+++ dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
@@ -9,9 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Developer 3" );
-         System.out.println( "added by test lead" );
-         System.out.println( "review comments" );
-        System.out.println( "Hello world!!!" );
-        System.out.println( "My First Program" );
     }
 }

6c100c604aabc434229f8be179b77f1ed2a606cc
danielraja <danny2mar91@gmail.com>
2018-01-04 13:21:10 +0530

    deleted sysout by developer 2

--- dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
+++ dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
@@ -8,11 +8,5 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World! 213" );
-        System.out.println( "Hello World! 124" );
-        System.out.println( "Hello World! 125" );
-        System.out.println("Developer 1");
-        System.out.println( "Developer 2" );
     }
 }

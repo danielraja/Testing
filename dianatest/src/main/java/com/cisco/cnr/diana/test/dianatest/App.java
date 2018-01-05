c1e19fb773917023aa4bc4057bbfe6195f5b6354
danielraja <danny2mar91@gmail.com>
2018-01-05 15:02:05 +0530

    Initial Commit by admin

--- dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
+++ dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
@@ -8,11 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Take care - admin");
-        System.out.println("Have a good day - developer...");
-        System.out.println("Added by developer One - Scope");
-        System.out.println("Zone added by developer 2");
-        System.out.println("Hello from Developer 1");
-        System.out.println("Good Afternoon from dev 2");
+        System.out.println("Hello World...");
     }
 }

b5748d70ccc0b2344e958ce04f4fac53a472d879
danielraja <danny2mar91@gmail.com>
2018-01-05 10:07:20 +0530

    deleted unwanted changes - admin

--- dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
+++ dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
@@ -8,12 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Developer 3" );
-        System.out.println( "check message" );
-        System.out.println( "my developer 1 change" );
-        System.out.println("Added by the actual developer from Eclipse...");
-        System.out.println( "added by the git admin..." );
-        System.out.println( "Second level changes from admin..." );
-        System.out.println("second level of change from the actual developer...");
+        System.out.println("Deleted unwanted statments...");
     }
 }

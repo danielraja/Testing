09d1c2c993f4631cf983c3f87169ba7ab84d3aab
danielraja <danny2mar91@gmail.com>
2018-01-05 10:14:36 +0530

    admin changes final

--- dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
+++ dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Deleted unwanted statments...");
+        System.out.println("final level of change from the aDMIN...");
     }
 }

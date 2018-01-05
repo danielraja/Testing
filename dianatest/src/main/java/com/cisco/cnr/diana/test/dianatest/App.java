d80165c776f248019b1a050897721f627c989bac
danny <danny2mar91@gmail.com>
2018-01-05 10:20:07 +0530

    hello message developer

--- dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
+++ dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Hello admin");
+        System.out.println("My hello message from developer...");
     }
 }

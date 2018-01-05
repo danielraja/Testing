b95cd5b4fd758eb39c36aeec898a808b454f92ce
danny <danny2mar91@gmail.com>
2018-01-05 10:34:11 +0530

    good day from developer 1

--- dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
+++ dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Take care - admin");
+        System.out.println("Have a good day - developer...");
     }
 }

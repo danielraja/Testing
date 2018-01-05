c88230d70b91e5329421bfbf2f30965d17586093
sugramal <sugramal@SUGRAMAL-EGE3J.partnet.cisco.com>
2018-01-05 18:59:51 +0530

    merge scenario - Dev 1

--- dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
+++ dianatest/src/main/java/com/cisco/cnr/diana/test/dianatest/App.java
@@ -12,5 +12,7 @@ public class App
         
         System.out.println("This is Dev1 Git Demo");
         System.out.println("This is Dev2 Git Demo Comments...");
+        
+        System.out.println("Changes made for merge scenario from Dev1");
     }
 }
